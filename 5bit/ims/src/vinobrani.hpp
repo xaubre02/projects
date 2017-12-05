b67905f4aab95a375682f05af614fad2fcbfe5bf
Tomáš Aubrecht <xaubre02@stud.fit.vutbr.cz>
2017-12-05 20:05:25 +0100

    Update vinobrani.hpp

--- 5bit/ims/src/vinobrani.hpp
+++ 5bit/ims/src/vinobrani.hpp
@@ -57,6 +57,8 @@ bool zpracovani_ukonceno = false;
 // Trida reprezentujici pracovnika
 class Pracovnik : public Process 
 {
+    double time;                // zaznamenani pocatecniho casu procesu
+    
     void Behavior(void);        // chovani pracovnika
     void Odzrnit(void);         // odzrneni jedne bedny hroznu
     void Lisovat(void);         // lisovani rmutu
@@ -67,6 +69,8 @@ class Pracovnik : public Process
 // Trida reprezentujici sberace
 class Sberac : public Process 
 {
+    double time;                // zaznamenani pocatecniho casu procesu
+    
     void Behavior(void);        // chovani sberace
     void Sklidit(void);         // sklizeni hroznu
     void Prepravit(void);       // prepraveni beden s hroznama ke zpracovani
