aea91912f31a0b71b58e57734c4d22ed51c8ea10
Aubrecht Tomas <xaubre02@merlin.fit.vutbr.cz>
2019-03-21 20:11:42 +0100

    PRL: test.sh almost done

--- mis/2mis/prl/projekt1/test.sh
+++ mis/2mis/prl/projekt1/test.sh
@@ -6,24 +6,34 @@
 # Login:   xaubre02
 # -----------------------
 
-if [ $# != 2 ]; then
+if [ $# != 1 ]; then
     echo "Chyba: neplatny pocet argumentu"
     echo "Pouziti: test.sh pocet_hodnot"
     exit 1
 fi
 
+# počet hodnot
 values=$1
-processors=$((2*$values-1))
-som=`echo "scale=2 ; l($numbers) / l(2)" | bc -l`	
+if [ $values -lt 1 ]; then
+    echo "Chyba: neplatny pocet hodnot - musi byt vetsi nez 0"
+    exit 1
+fi
 
-echo $som
+# počet procesorů
+if [ $values -eq 1 ]; then
+    procs=1
+else
+    procs=`echo "2 * l($values) / l(2) - 1" | bc -l`
+    # zaokrouhlení nahoru
+    procs=`echo $procs | awk '{print ($0-int($0)>0)?int($0)+1:int($0)}'`
+fi
 
 # vytvoření souboru numbers
-#dd if=/dev/random bs=1 count=$values of=numbers > /dev/null 2>&1
+dd if=/dev/random bs=1 count=$values of=numbers > /dev/null 2>&1
 
 # překlad a spuštění
 #mpicc --prefix /usr/local/share/OpenMPI bks.cpp -o bks -lm
 #mpirun --prefix /usr/local/share/OpenMPI -np $processors bks
 
 # vyčištění
-#rm -rf bks numbers
+rm -rf bks numbers
